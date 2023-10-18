a73a80f500daab16979fdaa13433e92ce1b85c02
walid oumechtak <woumechtak@gmail.com>
2023-10-18 03:26:26 -0700

    done

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * buuble_sort - sort an array of int with bubbel sort
+ * @array: the array
+ * @size: the size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	int	temp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1)
+		{
+			if (array[i] > array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			j++;
+		}
+		i++;
+	}
+}
