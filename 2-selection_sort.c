9da199d6ce870c3379ad0d60474656492bf3bf20
walid oumechtak <woumechtak@gmail.com>
2023-10-24 02:15:38 -0700

    done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,14 +10,19 @@ void selection_sort(int *array, size_t size)
 {
 	size_t	min_index;
 	int	temp;
+	size_t	i;
+	size_t	j;
 
-	for (size_t i = 0; i < size; i++)
+	i = 0;
+	while (i < size)
 	{
 		min_index = i;
-		for (size_t j = i + 1; j < size; j++)
+		j = 0;
+		while (j < size)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
+			j++;
 		}
 		if (min_index != i)
 		{
@@ -26,5 +31,6 @@ void selection_sort(int *array, size_t size)
 			array[min_index] = temp;
 			print_array(array, size);
 		}
+		i++;
 	}
 }
