ecaceaefd396db49dd9b915122207cc6e49a9da2
walid oumechtak <woumechtak@gmail.com>
2023-10-24 01:53:14 -0700

    done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,13 +8,13 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int	min_index;
+	size_t	min_index;
 	int	temp;
 
-	for (int i = 0; i < size; i++)
+	for (size_t i = 0; i < size; i++)
 	{
 		min_index = i;
-		for (int j = i + 1; j < size; j++)
+		for (size_t j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
